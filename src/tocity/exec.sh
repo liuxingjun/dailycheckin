f6915d62367f85d8b4b21d350b91533a2e662da1
liuxingjun <connliu@qq.com>
2023-05-25 06:34:06 +0000

    fix tocity url

--- src/tocity/exec.sh
+++ src/tocity/exec.sh
@@ -6,4 +6,4 @@ PWD=$(dirname "$(readlink -f "$0")")
 envsubst < $PWD/data.txt | curl -s -L -X POST \
 -H 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode @- \
-ttp://member.gph5.com/hq_member/Record!signIn
+http://member.gph5.com/hq_member/Record!signIn

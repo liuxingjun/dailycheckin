5b7947af3c4f7e8b861525d7098050276827cc0a
liuxingjun <connliu@qq.com>
2023-07-19 17:59:56 +0800

    Update  xgimi response check

--- src/xgimi/exec.sh
+++ src/xgimi/exec.sh
@@ -2,8 +2,13 @@
 set -e
 
 PWD=$(dirname "$(readlink -f "$0")")
+url="https://mobile-api.xgimi.com/app/v4/integral/signin"
+request='{"configNo":"2021061111211168"}'
+output=$(envsubst < "$PWD/headers.txt" | curl -s -L -X POST -H @- -d "$request" $url)
 
-envsubst < $PWD/headers.txt | curl -s -L -X POST \
--H @- \
--d '{"configNo":"2021061111211168"}' \
-https://mobile-api.xgimi.com/app/v4/integral/signin
+if ! jq -e '.code | test("ok")' <<< "$output" > /dev/null; then
+  echo "xgimi signin error:$output"
+  exit 1
+fi
+echo "xgimi signin succeed"
+exit 0
